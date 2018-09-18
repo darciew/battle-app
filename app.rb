dc67254b2611eb85bf2070bacccf93f5c1b431d1
Darcie <darciewalsh@hotmail.com>
2018-09-18 17:46:33 +0100

    added /play route

--- app.rb
+++ app.rb
@@ -1,7 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'something secret'
+
   get '/' do
-    'Hello Battle'
+    erb :player_input
+  end
+
+  post '/names' do
+
+    session[:Player1_Name] = params[:Player1_Name]
+    session[:Player2_Name] = params[:Player2_Name]
+  
+    redirect '/play'
+  end
+
+  get "/play" do
+    @Player1_Name = session[:Player1_Name]
+    @Player2_Name = session[:Player2_Name]
+    erb :players
   end
 end
