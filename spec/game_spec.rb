a2a7b1ae6e4faffc134bf18c913802d71f402646
Darcie <darciewalsh@hotmail.com>
2018-09-20 16:14:39 +0100

    Add tests for player objects in Game spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,20 +1,32 @@
 require 'game'
 
 describe Game do
-  let(:pokemon) { described_class.new(player1, player2) }
+  let(:pokemon) { described_class.new(tom, andy) }
 
-  let(:player1) {double :people, reduce_hp: nil }
-  let(:player2) { double :pikachu, reduce_hp: nil }
+  let(:tom) {double :player, reduce_hp: nil }
+  let(:andy) { double :player, reduce_hp: nil }
   # let(:player1) { double :pikachu, reduce_hp: nil }
+
+  describe 'player1' do
+    it 'should return the player1 object' do
+      expect(pokemon.player1).to eq tom
+    end
+  end
+
+  describe 'player2' do
+    it 'should return the player2 object' do
+      expect(pokemon.player2).to eq andy
+    end
+  end
 
   describe '#attack' do
     it 'reduces another players hp by 10' do
-      pokemon.attack(player2)
-      expect(player2).to have_received(:reduce_hp)
+      pokemon.attack(andy)
+      expect(andy).to have_received(:reduce_hp)
     end
     it 'reduces another players hp by 10' do
-      pokemon.attack(player1)
-      expect(player1).to have_received(:reduce_hp)
+      pokemon.attack(tom)
+      expect(tom).to have_received(:reduce_hp)
     end
   end
 end
